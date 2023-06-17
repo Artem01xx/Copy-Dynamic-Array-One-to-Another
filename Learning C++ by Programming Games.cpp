a47936b74b0c4e1f4bcfd2ee691570176de98bac
Artem01xx <131177303+Artem01xx@users.noreply.github.com>
2023-06-17 20:27:00 +0500

    Create Learning C++ by Programming Games.cpp

---
+++ Learning C++ by Programming Games.cpp
@@ -0,0 +1,68 @@
+﻿
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <fstream> 
+#include <memory>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+//Copy Dynamic Array One to Another
+
+
+void FeelArray(int* arr, int size)
+{
+	for (int i = 0;i < size;i++)
+	{
+		arr[i] = rand() % 20;
+	}
+}
+
+void PrintArray(const int* arr, const int size)
+{
+	cout << " First Array " << endl;
+
+	for (int i = 0;i < size;i++)
+	{
+		cout << arr[i] << endl;
+	}
+}
+
+int main()
+{
+    int size;
+	cin >> size;
+	cout << endl;
+	int* arr = new int[size];
+	
+	FeelArray(arr, size);
+	PrintArray(arr, size);
+	
+	int SecondSize = size + 1;            
+	int* SecondArr = new int[SecondSize];
+
+	for (int i = 0;i < size;i++)
+	{            
+		SecondArr[i] = arr[i];     
+	}
+	
+	cout << " Second Array With + 1 Number " << endl;
+
+	for (int i = 0;i < size + 1;i++)
+	{
+		SecondArr[SecondSize-1] = 600;          
+		cout << SecondArr[i] << endl;
+	}
+
+
+	delete[]SecondArr;
+	delete[]arr;
+
+
+	return 0;
+
+
+}
+
